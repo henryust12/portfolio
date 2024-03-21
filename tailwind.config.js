ea35ee58fcb4a8a15878debaa62a16f590d98c04
hendrinextmake <henryust12nextmake@gmail.com>
2024-03-21 22:23:19 +0900

    set tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./**/*.{html, css}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
